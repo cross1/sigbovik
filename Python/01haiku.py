ae7dbb9a65a9212c67cc4fb814ef4f6ded97b5cb
Giselle Serate <giselle.serate@gmail.com>
2017-06-30 21:51:12 -0700

    Format Python acrostic and haiku.

--- Python/01haiku.py
+++ Python/01haiku.py
@@ -1,3 +1,5 @@
-giraffe = [5, 1, 4] # 5
-elephant = giraffe.sort() # 7
-print(sorted(giraffe)) # 5
+# 3-line poem with 5-7-5 syllables on each line, respectively. 
+# -------------------------------------------------------------------------------------
+giraffe = [5, 1, 4]
+elephant = giraffe.sort()
+print(sorted(giraffe))
