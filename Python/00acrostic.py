7f83dbb7d56a6472ad9117fa498325e6da526d53
Giselle Serate <giselle.serate@gmail.com>
2017-06-30 21:45:47 -0700

    Write Python acrostic.

---
+++ Python/00acrostic.py
@@ -0,0 +1,10 @@
+# Notes: Does not actually return or print anything, but rabbit is sorted.
+rabbit = [9, 5, 6]
+# ------------------------------
+for hornbill in range(1, len(rabbit)):												# F
+		eagle = hornbill															# E
+		while eagle > 0 and rabbit[eagle] < rabbit[eagle-1]:						# W
+				eagle -= 1															# E
+				rabbit[eagle+1], rabbit[eagle] = rabbit[eagle], rabbit[eagle+1]		# R
+# -------------------------------
+print(rabbit) # Not part of the poem; proves sortedness. 
