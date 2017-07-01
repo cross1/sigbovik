708704031e2483959ebc67bb52d53d8fd6200e7f
Giselle Serate <giselle.serate@gmail.com>
2017-07-01 01:55:43 -0700

    Add problematic unfinished Python limerick. It's possible I'll come back.

---
+++ Python/04limerick.py
@@ -0,0 +1,13 @@
+# Definitely NOT complete. Pausing, may return. 
+# Note that this is my first rhyming poem in the Python series.
+# Note that it's also 1:55.
+
+earth = [eachPig for eachPig in mySty]
+sky = [eachPig for eachPig in theSky]
+for eachPlace in myFort:
+	eachPlace.sort()
+print("Oh dear, I have stepped in cow pies.")
+
+# print(sortPigs+" and "+sortCows+" .")
+
+# Honestly, I'm not sure I want to read this to figure out if this is actually a limerick.
